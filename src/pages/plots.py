f3e23f5c1747cd597f8a24770420ae88ecf7a0b5
abreham <aynuyeabresh@gmail.com>
2021-08-15 11:24:30 -0300

    Dashboard

--- src/pages/plots.py
+++ src/pages/plots.py
@@ -26,9 +26,31 @@ def write():
 
         duration_df = df[['duration', 'filename']]
         if plot == 'Durations':
-            print(df)
-            plot_dist(duration_df, 'duration')
-            st.pyplot()
+            st.header("Analysis of Audio Duration")
+            st.subheader("Distribution of Duration")
+            st.image('src/images/distribution_of _duration.png')
+            st.write("""
+            Most of the audio has a duration of between 2.25sec to 2.6sec.The max duration is 6.1s and the min duration is also 2.1s
+            """)
+            #print(df)
+            #plot_dist(duration_df, 'duration')
+            #st.pyplot()
 
             #plots.plot_hist(train, 'duration', 'orange')
-
+        elif plot == 'Outlier':
+            st.subheader("Outlier Detection for Duration")
+            st.image('src/images/outliner_distribution.png')
+            st.write("""
+            #### The above plot show us we have no outlier in the audio data duration
+            """)
+        elif plot == 'Translation Length':
+            st.header("Analysis of Audio Transcription")
+            st.subheader("Distribution of Transcription Length")
+            st.image('src/images/dist_of_trans_len.png')
+            st.write("""
+            Most of the audio has a transcription lenght of 40 characters. The max transcription length is 121characters  and the min transcription length is 5 characters
+            """)
+        elif plot == 'Character Per Seconds':
+            st.subheader("Distribution of Character per sec")
+            st.image('src/images\dist_char_per_sec.png')
+        
